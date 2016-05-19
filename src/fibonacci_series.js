2641a8df341c416418d78212694998081aa6923c
ZT1996 <294238532@qq.com>
2016-05-19 22:38:08 +0800

    test07

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function fibonacci_series(n) {
-    
+    var fibonacciArray = [];
+
+    for(var i = 0; i <= n; i++){
+        fibonacciArray.push(fibonacci(i));
+    }
+    return fibonacciArray;
 }
+
+function fibonacci(i) {
+    if(i <= 1){
+        return i;
+    }
+    else{
+        return fibonacci(i-1) + fibonacci(i-2);
+    }
+}
+
 
 module.exports = fibonacci_series;
